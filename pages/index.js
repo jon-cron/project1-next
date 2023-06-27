cdf30b2af71b4684c18e58018c1bbafb24b4d597
jon-cron <jonathanacm95@gmail.com>
2023-06-27 08:26:30 -0600

    push

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
+import { getFeaturedEvents } from "@/data/dummy-data";
+
 const HomePage = () => {
+  const featuredEvents = getFeaturedEvents();
+  console.log(featuredEvents);
   return (
     <div>
       <h1>Home page</h1>

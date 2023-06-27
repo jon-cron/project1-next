765c4ea6718d7101d43796fc0635bd574a49279d
jon-cron <jonathanacm95@gmail.com>
2023-06-27 10:00:58 -0600

    push

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,4 +1,12 @@
+import { getEventById } from "@/data/dummy-data";
+import { useRouter } from "next/router";
+
 const EventDetailPage = () => {
+  const router = useRouter();
+  const event = getEventById(router.query.id);
+  if (!event) {
+    return <p>no event found</p>;
+  }
   return (
     <div>
       <h1>event detail</h1>
